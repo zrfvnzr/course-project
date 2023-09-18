06073e5fd487cc7c1bb22c0b438547c872e365a0
Paolo <paolo.licup@catalisgov.com>
2023-09-19 00:45:00 +0800

    added content to the recipe components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
     selector: 'app-recipe-list',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-    recipes = [];
+    recipes: Recipe[] = [
+		new Recipe(
+			'Test Recipe',
+			'Test recipe description',
+			'https://hips.hearstapps.com/hmg-prod/images/crepes-index-64347419e3c7a.jpg'
+		)
+	];
 
     constructor() { }
 
