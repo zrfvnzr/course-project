57dcd88c2aa19670f4b650796bf33a5f4e06c9fc
Paolo <paolo.licup@catalisgov.com>
2023-09-20 01:10:02 +0800

    navigation with event binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'course-project';
+    title = 'course-project';
+
+	loadedFeature = 'recipe';
+
+	onNavigate(feature: string) {
+		this.loadedFeature = feature;
+	}
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+	@Output() featureSelected = new EventEmitter<string>();
 
-  ngOnInit(): void {
-  }
+	onSelect(feature: string) {
+		this.featureSelected.emit(feature);
+	}
+        
+    constructor() { }
+
+    ngOnInit(): void {
+    }
 
 }
