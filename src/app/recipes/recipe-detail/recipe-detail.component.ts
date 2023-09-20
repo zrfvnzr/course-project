fa96d80ce32a9efe67bf40897d26ae97561ebb62
Paolo <paolo.licup@catalisgov.com>
2023-09-21 03:56:29 +0800

    programmatic navigation to edit page

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
     selector: 'app-recipe-detail',
@@ -16,6 +16,7 @@ export class RecipeDetailComponent implements OnInit {
     constructor(
         private recipeService: RecipeService,
         private route: ActivatedRoute,
+        private router: Router,
     ) { }
 
     ngOnInit(): void {
@@ -30,6 +31,10 @@ export class RecipeDetailComponent implements OnInit {
 
     onAddToShoppingList() {
         this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+    }
+
+    onEditRecipe() {
+        this.router.navigate(['edit'], {relativeTo: this.route});
     }
 
 }
