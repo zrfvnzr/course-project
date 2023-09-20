ca4a3eb7d663cccf74b22a98d30399d8fa46d940
Paolo <paolo.licup@catalisgov.com>
2023-09-20 23:35:44 +0800

    adding ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
 
@@ -9,12 +10,20 @@ export class RecipeService {
 		new Recipe(
 			'Test Recipe',
 			'Test recipe description',
-			'https://hips.hearstapps.com/hmg-prod/images/crepes-index-64347419e3c7a.jpg'
+			'https://hips.hearstapps.com/hmg-prod/images/crepes-index-64347419e3c7a.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20),
+            ]
 		),
         new Recipe(
 			'Test Recipe 2',
 			'Test recipe description 2',
-			'https://hips.hearstapps.com/hmg-prod/images/crepes-index-64347419e3c7a.jpg'
+			'https://hips.hearstapps.com/hmg-prod/images/crepes-index-64347419e3c7a.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 3),
+            ]
 		)
 	];
 
