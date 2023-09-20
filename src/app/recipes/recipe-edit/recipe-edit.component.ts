5a340d894707a2c9accc66179902f54ffd375d1d
Paolo <paolo.licup@catalisgov.com>
2023-09-21 03:49:31 +0800

    retrieving route parameters

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
-  selector: 'app-recipe-edit',
-  templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.css']
+    selector: 'app-recipe-edit',
+    templateUrl: './recipe-edit.component.html',
+    styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
+	id: number | undefined;
+	editMode = false;
 
-  constructor() { }
+    constructor(private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.route.params
+			.subscribe(
+				(params: Params) => {
+					this.id = +params['id'];
+					this.editMode = params['id'] != null;
+				}
+			);
+    }
 
 }
