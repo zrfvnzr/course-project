0c11d954e672e8c43bedbb253d610e018046b2f5
Paolo <paolo.licup@catalisgov.com>
2023-09-21 02:48:06 +0800

    configuring route parameters

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -33,6 +33,10 @@ export class RecipeService {
         return this.recipes.slice();
     }
 
+    getRecipe(id: number) {
+        return this.recipes[id];
+    }
+
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.shoppingListService.addIngredients(ingredients);
     }
