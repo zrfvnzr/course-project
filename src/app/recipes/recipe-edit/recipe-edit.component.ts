943e2aa80c5864ac3189dac7b70b37e729b82f19
Paolo <paolo.licup@catalisgov.com>
2023-09-22 05:24:06 +0800

    added new ingredient controls

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -63,4 +63,13 @@ export class RecipeEditComponent implements OnInit {
         return (<FormArray>this.recipeForm.get('ingredients')).controls;
     }
 
+    onAddIngredient() {
+        (<FormArray>this.recipeForm.get('ingredients')).push(
+            new FormGroup({
+                'name': new FormControl(),
+                'amount': new FormControl(),
+            })
+        );
+    }
+
 }
