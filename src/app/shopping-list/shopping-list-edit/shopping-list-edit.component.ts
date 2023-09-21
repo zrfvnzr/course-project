3a31d9f69ac80ee267d814cdb1aa4d3d60df3f86
Paolo <paolo.licup@catalisgov.com>
2023-09-22 04:12:21 +0800

    resetting the form

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -37,7 +37,7 @@ export class ShoppingListEditComponent implements OnInit {
             );
     }
 
-	onAddItem(form: NgForm) {
+	onSubmit(form: NgForm) {
         const value = form.value;
 		const newIngredient = new Ingredient(value.name, value.amount);
         if (this.editMode) {
@@ -45,6 +45,8 @@ export class ShoppingListEditComponent implements OnInit {
         } else {
             this.shoppingListService.addIngredient(newIngredient);
         }
+        this.editMode = false;
+        form.reset();
 	}
 
     ngOnDestroy() {
