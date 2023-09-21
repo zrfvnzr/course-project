1f3af56d0d85db3d67fed0bfcae900e7f8b1de60
Paolo <paolo.licup@catalisgov.com>
2023-09-22 04:08:55 +0800

    updating existing item

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -40,7 +40,11 @@ export class ShoppingListEditComponent implements OnInit {
 	onAddItem(form: NgForm) {
         const value = form.value;
 		const newIngredient = new Ingredient(value.name, value.amount);
-        this.shoppingListService.addIngredient(newIngredient);
+        if (this.editMode) {
+            this.shoppingListService.updateIngredient(this.editedItemIndex ?? 0, newIngredient);
+        } else {
+            this.shoppingListService.addIngredient(newIngredient);
+        }
 	}
 
     ngOnDestroy() {
