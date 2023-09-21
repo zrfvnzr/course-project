656ad3994bf567b3d887ddb879e1b8ffc2d8dde8
Paolo <paolo.licup@catalisgov.com>
2023-09-22 05:40:20 +0800

    submitting the recipe edit form

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -20,6 +20,12 @@ export class RecipeListComponent implements OnInit {
 
     ngOnInit(): void {
         this.recipes = this.recipeService.getRecipes();
+        this.recipeService.recipesChanged
+            .subscribe(
+                (recipes: Recipe[]) => {
+                    this.recipes = recipes;
+                }
+            )
     }
 
     onNewRecipe() {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,9 +2,12 @@ import { Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Subject } from "rxjs";
 
 @Injectable()
 export class RecipeService {
+
+    recipesChanged = new Subject<Recipe[]>;
 
     private recipes: Recipe[] = [
 		new Recipe(
@@ -37,6 +40,16 @@ export class RecipeService {
 
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.shoppingListService.addIngredients(ingredients);
+    }
+
+    addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
     }
 
     constructor(private shoppingListService: ShoppingListService) {}
