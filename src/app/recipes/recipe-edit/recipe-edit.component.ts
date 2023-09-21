956f81f8e7c0df97e7d01abd96226ef16ce2e899
Paolo <paolo.licup@catalisgov.com>
2023-09-22 05:19:11 +0800

    added ingredient controls to form array

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 
@@ -30,21 +30,37 @@ export class RecipeEditComponent implements OnInit {
         let recipeName = '';
         let recipeImagePath = '';
         let recipeDescription = '';
+        let recipeIngredients = new FormArray<any>([]);
         if (this.editMode) {
             const recipe = this.recipeService.getRecipe(this.id ?? 0);
             recipeName = recipe.name;
             recipeImagePath = recipe.imagePath;
             recipeDescription = recipe.description;
+            if (recipe['ingredients']) {
+                for (let ingredient of recipe.ingredients) {
+                    recipeIngredients.push(
+                        new FormGroup({
+                            'name': new FormControl(ingredient.name),
+                            'amount': new FormControl(ingredient.amount)
+                        })
+                    );
+                }
+            }
         }
         this.recipeForm = new FormGroup({
             'name': new FormControl(recipeName),
             'imagePath': new FormControl(recipeImagePath),
             'description': new FormControl(recipeDescription),
+            'ingredients': recipeIngredients,
         });
     }
 
     onSubmit() {
         console.log(this.recipeForm);
+    }
+
+    get controls() {
+        return (<FormArray>this.recipeForm.get('ingredients')).controls;
     }
 
 }
