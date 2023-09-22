3f128692e1f6f73a2ceefe36ae1bad06a4e10c68
Paolo <paolo.licup@catalisgov.com>
2023-09-23 03:59:16 +0800

    storing recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,21 @@
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-    constructor(private httpClient: HttpClientModule) {}
+
+    constructor(
+        private httpClient: HttpClient,
+        private recipeService: RecipeService,
+    ) {}
+
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+        this.httpClient.put('localhost:8080/api/recipes', recipes)
+            .subscribe((response: any) => {
+                console.log(response);
+            });
+    }
+
 }
