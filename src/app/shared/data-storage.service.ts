cd098ad6164356d076c2084d5d7e9bf7857cb17b
Paolo <paolo.licup@catalisgov.com>
2023-09-23 04:17:01 +0800

    transforming response data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
+import { map } from "rxjs/operators";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -22,6 +23,11 @@ export class DataStorageService {
     fetchRecipes() {
         this.httpClient
             .get<Recipe[]>('localhost:8080/api/recipes')
+            .pipe(map(recipes => {
+                return recipes.map(recipe => {
+                    return {...recipe, ingredients: recipe.ingredients ?? []};
+                });
+            }))
             .subscribe(recipes => {
                 this.recipeService.setRecipes(recipes);
             });
